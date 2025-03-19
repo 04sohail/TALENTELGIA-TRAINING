13f64db61a6d91225ceacaf06c5d78c0377a100a
Sk Sohail <sksohail44451@gmail.com>
2025-03-19 11:18:14 +0530

    ADD WEEK_6 DAY_2 INITIAL REACT + TYPESCRIPT SETUP WITH VITE

---
+++ WEEK_6/DAY_2/pagination/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [currentPage, updateCurrentPage] = useState(1)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
