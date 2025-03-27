3a0a71020fbf884946dce22a6e4eda909abe195f
Sk Sohail <sksohail44451@gmail.com>
2025-03-27 09:08:47 +0530

    Add initial setup for E-Commerce project with React, TypeScript, and Vite

---
+++ WEEK_7/DAY_3/E-COMMERCE/e-commerce/src/pages/Landing_Page.tsx
@@ -0,0 +1,10 @@
+
+import Header from '../components/Header'
+
+const landing_Page = () => {
+  return (
+    <Header />
+  )
+}
+
+export default landing_Page
---
+++ WEEK_7/DAY_3/E-COMMERCE/e-commerce/src/types/Product_types.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Product_types = () => {
+    return (
+        <div>Product_types</div>
+    )
+}
+
+export default Product_types
